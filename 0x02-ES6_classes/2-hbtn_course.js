bf4d57095b39d66c236e496340ce3a09add972c2
FadiliAnas <anasfadili65@gmail.com>
2025-06-25 02:36:37 +0200

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,42 +1,61 @@
-export default class ALXCourse {
+#!/usr/bin/node
+
+export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number' || Number.isNaN(length)) {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
     }
 
+    // eslint-disable-next-line no-underscore-dangle
     this._name = name;
+    // eslint-disable-next-line no-underscore-dangle
     this._length = length;
+    // eslint-disable-next-line no-underscore-dangle
     this._students = students;
   }
 
   get name() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._name;
   }
 
   set name(value) {
-    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    // eslint-disable-next-line no-underscore-dangle
     this._name = value;
   }
 
   get length() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._length;
   }
 
   set length(value) {
-    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw new TypeError('Length must be a number');
+    }
+    // eslint-disable-next-line no-underscore-dangle
     this._length = value;
   }
 
   get students() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._students;
   }
 
   set students(value) {
-    if (!Array.isArray(value) || !value.every((s) => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._students = value;
   }
 }
